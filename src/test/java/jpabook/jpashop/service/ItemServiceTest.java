4d6415720f2b4fe8d791563788c9c391bbba3fcd
Leehyoju97 <dlgywn2420@gmail.com>
2023-02-09 14:28:09 +0900

    상품 등록,조회,수정 기능추가

---
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void 상품등록() {
+
+        Item item = new Book();
+        item.setName("jpa");
+
+        Long saveId = itemService.saveItem(item);
+
+        assertEquals(item, itemRepository.findOne(saveId));
+    }
+
+}
