fd571e51ce0a703b4de13644e7e6b9148add060a
Leehyoju97 <dlgywn2420@gmail.com>
2023-02-09 14:28:09 +0900

    상품 등록,조회,수정 기능추가

---
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+
+        if (restStock < 0) {
+            throw new NotEnoughStockException("상품 재고가 부족하다.");
+        }
+    }
+
+}
